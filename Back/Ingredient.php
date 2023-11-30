ff48abf1db166058adda7fccb2003a4fb490297f
SmiledPolecat34 <versayo03@gmail.com>
2023-11-30 12:51:15 +0100

    Delete unnecessary code for retrieving recipes by category

--- Back/Ingredient.php
+++ Back/Ingredient.php
@@ -1,26 +1,26 @@
-<?php
+    <?php
 
-// Ingredient.php
+    // Ingredient.php
 
-class Ingredient {
-    private $id;
-    private $name;
+    class Ingredient {
+        private $id;
+        private $name;
 
-    public function getId() {
-        return $this->id;
+        public function getId() {
+            return $this->id;
+        }
+
+        public function getName() {
+            return $this->name;
+        }
+
+        public function setId($id) {
+            $this->id = $id;
+        }
+
+        public function setName($name) {
+            $this->name = $name;
+        }
     }
 
-    public function getName() {
-        return $this->name;
-    }
-
-    public function setId($id) {
-        $this->id = $id;
-    }
-
-    public function setName($name) {
-        $this->name = $name;
-    }
-}
-
-?>
+    ?>
